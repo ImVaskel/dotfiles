66a6e7a4291ca10b5db6a83e6d3501808cc8b3ea
ImVaskel <49348256+ImVaskel@users.noreply.github.com>
2024-03-12 00:00:08 -0400

    socket cleanup, starship fix, fish plugin

--- .config/fish/1pw-bridge.fish
+++ .config/fish/1pw-bridge.fish
@@ -2,8 +2,21 @@
 # Adapted from https://dev.to/d4vsanchez/use-1password-ssh-agent-in-wsl-2j6m but fish:tm: (and more cursed)
 
 # set the ssh socket first
-# we don't really want to worry about the socket, so.
+# we don't want to share the socket, as it may exist but there is no command behind it.
 set -gx SSH_AUTH_SOCK /tmp/ssh-agent-$fish_pid.tmp
+
+# Cleans up the socat and temp sockets.
+function cleanup-ssh-agent --on-event fish_exit
+    if test -e $SSH_AUTH_SOCK
+        if test -S $SSH_AUTH_SOCK
+            rm $SSH_AUTH_SOCK
+        end
+    end
+    # TODO: Check if this is the best way of grepping socat.
+    if pgrep -q "socat"
+        killall "socat"
+    end
+end
 
 # reenable this if you're having issues with the relay and need to debug.
 # echo "Starting the ssh-agent relay" >&2
--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,19 +1,24 @@
 set fish_greeting
 
 set -x EDITOR $(which nvim)
+fish_add_path $HOME/.local/bin
 
-# Global, then local
+# Anything here is run before the local config.
 if status is-interactive
-    # Starship
-    function starship_transient_prompt_func
-        starship module character
-    end
-    starship init fish | source
-    enable_transience
+
 end
 
 set local_config ~/.config/fish/config.local.fish
 test -r $local_config; and source $local_config
 
-fish_add_path $HOME/.local/bin
-
+# Anything here is run after the local config
+if status is-interactive
+    # Due to starship being installed via brew, I need to init starship here after the script is run.
+    if command -q starship
+        function starship_transient_prompt_func
+            starship module character
+        end
+        starship init fish | source
+        enable_transience
+    end
+end
